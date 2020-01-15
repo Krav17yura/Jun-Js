e60cbfe1d59a9f55a7c897ad519234ae415cedf7
Yura <krav17yura@gmail.com>
2020-01-15 17:10:37 +0200

    First task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 'use strict';
 
-let stri = "lol";
-let num = 2;
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
 
-console.log(stri);
-console.log(num);
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
 
-console.log("kek");
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется");
+
+    appData.expenses[a1] = a2;
+    appData.expenses[a3] = a4;
+
+    alert ( "Бюджет на один день" + appData.budget/30);
