e861581d5125025aafb65dde6d71671f245cc084
Yura <krav17yura@gmail.com>
2020-01-30 18:43:59 +0200

    Task 13 AJAX Forms

--- js/script.js
+++ js/script.js
@@ -1,167 +1,257 @@
-'use strict';
+ window.addEventListener("DOMContentLoaded", function () {
+     'use strict';
 
-let statrButton = document.getElementById("start"),
+     // Tab ******************************************************************************************************************
 
-	budgetValue = document.querySelector(".budget-value"),
-	dayBudgetValue = document.querySelector(".daybudget-value"),
-	levelValue = document.querySelector(".level-value"),
-	expensesValue = document.querySelector(".expenses-value"),
-	optionalExpensesValue = document.querySelector(".optionalexpenses-value"),
-	incomValue = document.querySelector(".income-value"),
-	monthsavingsValue = document.querySelector(".monthsavings-value"),
-	yearsavingsValue = document.querySelector(".yearsavings-value"),
-
-	expensesItem = document.getElementsByClassName("expenses-item"),
-
-	buttonExpenses = document.getElementsByTagName("button")[0],
-	buttonOptionalExpenses = document.getElementsByTagName("button")[1],
-	buttonCountBudget = document.querySelector(".count-budget-btn"),
-
-	optionalexpensesItem = document.querySelectorAll(".optionalexpenses-item"),
-	chooseIncome = document.querySelector(".choose-income"),
-	savings = document.querySelector("#savings"),
-	chooseSum = document.querySelector(".choose-sum"),
-	choosePercent = document.querySelector(".choose-percent"),
-	year = document.querySelector(".year-value"),
-	month = document.querySelector(".month-value"),
-	day = document.querySelector(".day-value");
+     let infoHeader = document.querySelector('.info-header'),
+         infoHeaderTab = document.querySelectorAll('.info-header-tab'),
+         infoTabContent = document.querySelectorAll('.info-tabcontent');
 
 
+     function hideTabContent(a) {
+         for (let i = a; i < infoTabContent.length; i++) {
+             infoTabContent[i].classList.remove('show');
+             infoTabContent[i].classList.add("hide");
+         }
+     }
+
+     hideTabContent(1);
+
+     function showTabContent(b) {
+         if (infoTabContent[b].classList.contains("hide")) {
+             infoTabContent[b].classList.remove('hide');
+             infoTabContent[b].classList.add("show");
+         }
+     }
 
 
+     infoHeader.addEventListener('click', function (event) {
+         let target = event.target;
+         if (target && target.classList.contains("info-header-tab")) {
+             for (let i = 0; i < infoHeaderTab.length; i++) {
+                 if (target == infoHeaderTab[i]) {
+                     hideTabContent(0);
+                     showTabContent(i);
+                     break;
+                 }
+             }
+         }
+
+     });
+     // ************************************************************************************************************************       
+     // Timer ******************************************************************************************************************
+     let deadLine = '2020-01-31';
+
+     function getTimerDate(endtime) {
+         let t = Date.parse(endtime) - Date.parse(new Date()),
+             seconds = Math.floor((t / 1000) % 60),
+             minutes = Math.floor((t / 1000 / 60) % 60),
+             hours = Math.floor((t / (1000 * 60 * 60)));
+
+         return {
+             'total': t,
+             'seconds': seconds,
+             "minutes": minutes,
+             "hours": hours
+         };
+     }
 
 
-let money,
-	time;
+     function setTimerDate(id, endtime) {
+         let timer = document.getElementById(id),
+             hours = document.querySelector(".hours"),
+             minutes = document.querySelector(".minutes"),
+             seconds = document.querySelector(".seconds"),
+             timeInterval = setInterval(updateImterval, 1000);
 
-	buttonExpenses.disabled = true;
-	buttonOptionalExpenses.disabled = true;
-	buttonCountBudget.disabled = true;
+         function firstNumberZero(num) {
+             if (num <= 9) {
+                 return "0" + num;
+             } else {
+                 return num;
+             }
+         }
 
-statrButton.addEventListener('click', function () {
-	time = prompt("Введите дату в формате YYYY-MM-DD");
-	money = +prompt("Ваш бюджет на месяц?");
+         function updateImterval() {
+             let t = getTimerDate(endtime);
+             hours.textContent = firstNumberZero(t.hours);
+             minutes.textContent = firstNumberZero(t.minutes);
+             seconds.textContent = firstNumberZero(t.seconds);
 
-	while (isNaN(money) || money == "" || money == null) {
-		money = +prompt("Ваш бюджет на месяц?");
-	}
-	appData.timeData = time;
-	appData.budget = money;
-	budgetValue.textContent = money.toFixed();
-	year.value = new Date(Date.parse(time)).getFullYear();
-	month.value = new Date(Date.parse(time)).getMonth() + 1;
-	day.value = new Date(Date.parse(time)).getDay();
+             if (t.total <= 0) {
+                 clearInterval(timeInterval);
 
-	buttonExpenses.disabled = false;
-	buttonOptionalExpenses.disabled = false;
-	buttonCountBudget.disabled = false;
-});
+                 hours.textContent = "00";
+                 minutes.textContent = "00";
+                 seconds.textContent = "00";
+             }
 
+         }
 
-buttonExpenses.addEventListener('click', function () {
-	let sum = 0;
+     }
 
-	for (let i = 0; i < expensesItem.length; i++) {
-		let a = expensesItem[i].value,
-			b = expensesItem[++i].value;
+     setTimerDate("timer", deadLine);
 
-		if (typeof (a) === 'string' && typeof (a) != null && a != '' &&
-			typeof (b) != null && b != '' && a.length < 50) {
-			console.log("done");
-			appData.expenses[a] = b;
-			sum += +b;
-		} else {
-			console.log("error");
-			i = i - 1;
+     //********************************************************************************************************************
 
-		}
-	}
-	expensesValue.textContent = sum;
-});
-
-buttonOptionalExpenses.addEventListener('click', function () {
-	for (let i = 0; i < optionalexpensesItem.length; i++) {
-		let opt = optionalexpensesItem[i].value;
-		appData.optionalExpenses[i] = opt;
-		optionalExpensesValue.textContent += appData.optionalExpenses[i] + " ";
-	}
-
-});
-
-buttonCountBudget.addEventListener("click", function () {
-	if (appData.budget != undefined) {
-		appData.moneyPerDay = ((appData.budget - +expensesValue.textContent )/ 30).toFixed(1);
-		dayBudgetValue.textContent = appData.moneyPerDay;
+     // Modal window 
 
 
-		if (appData.moneyPerDay < 100) {
-			levelValue.textContent = "У вас низкий доход";
-		} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
-			levelValue.textContent = "У вас средний доход";
-		} else if (appData.moneyPerDay >= 2000) {
-			levelValue.textContent = "У вас высокий доход";
-		} else {
-			levelValue.textContent = "Произошла ошибка";
-		}
-	} else {
-		budgetValue.textContent = "Произошла ошибка";
-	}
+     let moreBtn = document.querySelector(".more"),
+         overlay = document.querySelector(".overlay"),
+         closeBtn = document.querySelector(".popup-close");
 
-});
+     moreBtn.addEventListener("click", function () {
+         overlay.style.display = "block";
+         this.classList.add("more-splash'");
+         document.body.style.overflow = 'hidden';
 
-chooseIncome.addEventListener("input", function () {
+     });
 
-	let items = chooseIncome.value;
-
-	appData.income = items.split(',');
-	incomValue.textContent = appData.income;
-
-});
-
-savings.addEventListener('click', function () {
-	if (appData.savings == true) {
-		appData.savings = false;
-	} else {
-		appData.savings = true;
-	}
-});
-
-chooseSum.addEventListener('input', function () {
-	if (appData.savings == true) {
-		let sum = chooseSum.value,
-			percent = choosePercent.value;
-		appData.monthIncome = (sum / 100 / 12 * percent).toFixed(1);
-		appData.yearIncome = (sum / 100 * percent).toFixed(1);
-
-		monthsavingsValue.textContent = appData.monthIncome;
-		yearsavingsValue.textContent = appData.yearIncome;
-
-	}
-});
-
-choosePercent.addEventListener('input', function () {
-	if (appData.savings == true) {
-		let sum = chooseSum.value,
-			percent = choosePercent.value;
-		appData.monthIncome = (sum / 100 / 12 * percent).toFixed(1);
-		appData.yearIncome = (sum / 100 * percent).toFixed(1);
-
-		monthsavingsValue.textContent = appData.monthIncome;
-		yearsavingsValue.textContent = appData.yearIncome;
-
-	}
-});
-
-let appData = {
-	budget: money,
-	timeData: time,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	savings: false,
-};
+     closeBtn.addEventListener("click", function () {
+         overlay.style.display = "none";
+         moreBtn.classList.remove("more-splash'");
+         document.body.style.overflow = '';
+     });
 
 
-for (let key in appData) {
-	console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
-}
+     /*   class options {
+           constructor(height,width,bg,fontSize,textAlign){
+               this.height = height;
+               this.width = width;
+               this.bg = bg;
+               this.fontSize = fontSize;
+               this.textAlign = textAlign;
+           }
+           
+            createDiv (){
+          let elem = document.createElement('div');
+          document.body.appendChild(elem);
+
+          let parametr = `height:${this.height}px;width:${this.width}px;background-color:${this.bg};font-size:${this.fontSize}px;text-align:${this.textAlign}`;
+          elem.style.cssText = parametr;
+           }
+
+       }
+
+       let item = new options(300, 300, "green", 30, "right");
+       item.createDiv(); */
+
+     //json
+     /*  let option = {
+          width: 1366,
+          height: 768,
+          background: 'red',
+          font : {
+              size: "16px",
+              color: "red"
+          }
+      };
+
+      let kek = 'lol';
+
+      console.log(JSON.stringify(kek));
+      console.log(JSON.stringify(option));
+      console.log(JSON.parse(JSON.stringify(option))); */
+
+
+     //  ModalFrom
+
+     let message = {
+         loading: "Загрузка",
+         succesful: "Спасибо, ваша заявка принята",
+         fail: "ERROR"
+     };
+
+     let form = document.querySelector(".main-form"),
+         input = document.querySelectorAll(".popup-form__input"),
+         statusMassage = document.createElement("div");
+
+     statusMassage.classList.add("status");
+
+     form.addEventListener("submit", function (event) {
+         event.preventDefault();
+         form.appendChild(statusMassage);
+
+         let request = new XMLHttpRequest();
+
+         request.open('POST', 'server.php');
+         request.setRequestHeader('Content-type', 'aplication/x-www-form-urlencoded');
+         //  request.setRequestHeader('Content-tupe', 'aplication/json');
+         let formData = new FormData(form);
+
+         //  let obj = {};
+         //  formData.forEach(function(value,key){
+         //      obj[key] = value;
+         //  });
+         // let json = JSON.parse(obj);
+
+         //  request.send(json);
+         request.send(formData);
+
+         request.addEventListener("readystatechange", function () {
+             if (request.readyState < 4) {
+                 statusMassage.innerHTML = message.loading;
+             } else if (request.readyState === 4 && request.status == 200) {
+                 statusMassage.innerHTML = message.succesful;
+             } else {
+                 statusMassage.innerHTML = message.fail;
+             }
+         });
+
+         for (let i = 0; i < input.length; i++) {
+             input[i].value = "";
+         }
+     });
+
+     // Form 
+
+     let massage = {
+         loading: "Заргрузка",
+         succesful: "Спасибо, мы скоро с вами свяжемся",
+         fail: "Error"
+     };
+
+
+     let forma = document.querySelector("#form"),
+         inputForm = document.querySelectorAll(".form-last"),
+         statusText = document.createElement("div");
+
+         statusText.classList.add("status");
+
+         forma.addEventListener("submit", function(event){
+            event.preventDefault();
+            forma.appendChild(statusText);
+
+            let request = new XMLHttpRequest();
+
+            request.open('Post', 'server.php');
+            request.setRequestHeader('Content-type', 'aplication/x-www-form-urlencoded');
+            let formInfa = new FormData(forma);
+            
+
+            
+         
+            request.send(formInfa);
+
+
+            request.addEventListener("readystatechange", function(){
+               if(request.readyState < 4){
+                   statusText.innerHTML = massage.loading;
+               }else if(request.readyState === 4 && request.status == 200){
+                   statusText.innerHTML = massage.succesful;
+               }else{
+                   statusText.innerHTML = massage.fail;
+               }
+            });
+
+            for(let i = 0; i<inputForm.length; i++){
+                inputForm[i].value = "";
+            }
+
+         });
+         
+
+         
+
+ });
