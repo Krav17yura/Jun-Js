9e2bd60399ba4fa8b1bdb0e16ff3ebd93854e1ca
Yura <krav17yura@gmail.com>
2020-02-05 15:12:00 +0200

    Task 15 Slider

--- js/script.js
+++ js/script.js
@@ -115,6 +115,8 @@
          document.body.style.overflow = '';
      });
 
+      //********************************************************************************************************************
+
 
      /*   class options {
            constructor(height,width,bg,fontSize,textAlign){
@@ -265,7 +267,61 @@
             .then(clearPole);
 
      });
+ //********************************************************************************************************************
+ // Slider
 
+        let sliderIndex = 1,
+            slides = document.querySelectorAll(".slider-item"),
+            next = document.querySelector(".next"),
+            prev = document.querySelector(".prev"),
+            sliderDots = document.querySelector(".slider-dots"),
+            dots = document.querySelectorAll(".dot");
+
+            showSlides(sliderIndex);
+             
+            function showSlides(n){
+                     if (n > slides.length){
+                        sliderIndex = 1;
+                     }
+                     if(n < 1){
+                        sliderIndex = slides.length;
+                     }
+
+
+                slides.forEach((item)=> item.style.display = "none");
+                dots.forEach((item)=> item.classList.remove("dot-active"));
+
+                slides[sliderIndex - 1].style.display = "block";
+                dots[sliderIndex - 1].classList.add("dot-active");
+
+
+            }
+
+            function nextSlidetr(n){
+                showSlides(sliderIndex += n);
+            }
+
+            function prevSlider(n){
+                showSlides(sliderIndex = n);
+            }
+
+            next.addEventListener('click', function(){
+                nextSlidetr(1);
+            });
+
+            prev.addEventListener('click', function(){
+                nextSlidetr(-1);
+            });
+
+            sliderDots.addEventListener('click', function(e){
+                for(let i = 0; i <dots.length + 1; i++){
+                    if(e.target.classList.contains('dot')&& e.target == dots[i-1]){
+                        prevSlider(i);
+                    }
+                }
+            });
+
+           
 
 
 
